71c5ca8731cb24bbea3fcc65128ea7e7238de5b5
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-03-19 13:40:35 +0100

    6gyak első feladat CSS nélkül

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,12 @@
+﻿function számos() {
+    var divnagy = document.createElement("div");
+    divnagy.id = "divnagy";
+    document.body.appendChild(divnagy);
+    for (var x = 0; x < 10; x++) {
+        var div = document.createElement("div");
+        div.innerHTML = x + 1;
+        //div.style.backgroundColor = rgb(100, 100, 100);
+        divnagy.appendChild(div);
+    }
+
+}
