51b7e9e7e12194e45b6ac9f21a66d51a5916a077
Benedek <benedek.nervetti@stud.uni-corvinus.hu>
2021-03-20 20:45:16 +0100

    Gyak6

--- HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -5,7 +5,8 @@
     for (var x = 0; x < 10; x++) {
         var div = document.createElement("div");
         div.innerHTML = x + 1;
-        //div.style.backgroundColor = rgb(100, 100, 100);
+        div.classList.add("egymás_mellé");
+        div.style.backgroundColor = "rgb(100, 100, 100)";
         divnagy.appendChild(div);
     }
 
